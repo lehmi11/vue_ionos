96159f283a4d58851d31c4b78c4f0db6cd437b91
PL <pl@.com>
2022-03-15 15:46:11 +0100

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
-<script setup>
-
+<script>
+  import HeroCard from '@/components/HeroCard.vue';
+  import Footer from '@/components/Footer.vue';
+  export default {
+    components: {
+      HeroCard, 
+      Footer
+    }
+  }
 </script>
 
 <template>
+  <main>
+    <HeroCard />
+  </main>
+    <Footer />
 
 </template>
 
-<style>
+<style lang="scss">
+
+@font-face {
+  font-family: $roboto;
+  src: url('@/assets/fonts/Roboto-Regular.ttf');
+  font-weight: 500;
+}
+
+body,
+html {
+  background-color: $second;
+  font-size: 10px;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+* {
+  box-sizing: border-box;
+}
 
 </style>
---
+++ src/components/HeroCard.vue
@@ -0,0 +1,33 @@
+
+<script>
+  import HeroCardImage from '@/components/HeroCardImage.vue';
+  import HeroCardIntro from '@/components/HeroCardIntro.vue';
+  export default {
+    components: {
+      HeroCardImage,
+      HeroCardIntro
+    }
+  }
+  
+
+</script>
+
+<template>
+  
+
+  <section class="hero-card">
+    <HeroCardImage />
+    <HeroCardIntro />
+  </section>
+
+</template>
+
+
+
+<style lang="scss" scoped>
+  .hero-card {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+  }
+</style>
